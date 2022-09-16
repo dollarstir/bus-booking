18fa41d56a5e0bcdb43713cb449ecd3447cf5e08
Frederick Ennin <kpin463@gmail.com>
2022-09-16 17:25:45 +0000

    booking 5

--- lib/initialapp.dart
+++ lib/initialapp.dart
@@ -22,24 +22,37 @@ class _InitialAppState extends State<InitialApp> {
                 Expanded(
                   flex: 1,
                   child: Container(
-                    padding: EdgeInsets.only(top: 5, right: 5, left: 5),
+                    padding: EdgeInsets.only(top: 2, right: 2, left: 2),
                     margin: EdgeInsets.only(left: 1, right: 1, top: 20),
                     // color: Colors.orange,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        // ClipRRect(
+                        //   borderRadius: BorderRadius.circular(20.0),
+                        //   child: Image.asset('assets/images/bus2.jpg', width: 150,
+                        //   height: 200,),
+                        // ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.4,
+                          child:Image.asset('assets/images/bus2.jpg'),
+                          decoration: new BoxDecoration(
+                            // color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
 
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image.asset('assets/images/bus2.jpg', width: 150,
-                          height: 200,),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.4,
+                          child:Image.asset('assets/images/plane4.png'),
+                          decoration: new BoxDecoration(
+                            // color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
                         ),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image.asset('assets/images/plane4.png', width: 150,
-                          height: 200,),
-                        ),
-                        
+                       
                       ],
                     ),
                   ),
@@ -47,20 +60,55 @@ class _InitialAppState extends State<InitialApp> {
                 Expanded(
                   flex: 1,
                   child: Container(
-                    color: Colors.green,
+                    // color: Colors.green,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Container(
                           margin: EdgeInsets.only(top: 20),
-                          child: Text("Welcome to Icounsel-Gh",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
-                          ),),
+                          child: Text(
+                            "Bus & Air Ticket Booking",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: Text(
+                            "Book your bus and air tickets with us is very easy and fast",
+                            style: TextStyle(
+                              fontSize: 18,
+                              // fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.15),
+                        GestureDetector(
+                          onTap: () async {
+                            //  Route route = MaterialPageRoute(builder: (c) => InitialTanx());
+                            //    Navigator.push(context, route);
+                          },
+                          child: Container(
+                              //padding: EdgeInsets.all(20),
+                              margin: EdgeInsets.all(10),
+                              height: 50,
+                              width: double.infinity,
+                              child: Center(
+                                child: Text(
+                                  "Get Started",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).accentColor,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              )),
                         ),
                       ],
-                      
                     ),
                   ),
                 ),
